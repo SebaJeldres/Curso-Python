7e8be8f0e16bc344c537d69b331f732528d788d1
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-07-25 21:01:20 -0400

    Clase 3: Asignaciones multiples

--- Clase_1_a_10.py
+++ Clase_1_a_10.py
@@ -1,47 +1,45 @@
-#--------------------------------------------------------
-#Clase 1
-#--------------------------------------------------------
-#print('Me encanta la pizza')
-#print('Es muy bueno')
+# --------------------------------------------------------
+# Clase 1
+# --------------------------------------------------------
+# print('Me encanta la pizza')
+# print('Es muy bueno')
 
 
-#--------------------------------------------------------
-#clase 2
-#--------------------------------------------------------
+# --------------------------------------------------------
+# clase 2 Variebles y tipos de datos
+# --------------------------------------------------------
 
-#Variebles y tipos de datos
+# String
+# nombre = 'Sebastian' #String
+# print('Hola ' + nombre)
+# print(type(nombre)) #Muestra el tipo de dato
 
-#String
-#nombre = 'Sebastian' #String
-#print('Hola ' + nombre)
-#print(type(nombre)) #Muestra el tipo de dato
+# apellido = 'Jeldes' #String
+# nombre_completo = 'Hola ' + nombre + apellido
+# print(nombre_completo)
 
-#apellido = 'Jeldes' #String
-#nombre_completo = 'Hola ' + nombre + apellido
-#print(nombre_completo)
+# entero
+# edad = 21 #int
+# edad2 = '21'
+# #edad = edad + 1
+# edad += 1
+# edad2 += 1 da error
+# una es un int y lña otra es un string
+# print(type(edad))
+# print(type(edad2))
 
-#entero
-edad = 21 #int
-edad2 = '21'
-#edad = edad + 1
-edad += 1
-#edad2 += 1 da error
-#una es un int y lña otra es un string
-#print(type(edad))
-#print(type(edad2))
+# entero con cadena de texto
+# print('Tu edad es ' + str(edad))
 
-#entero con cadena de texto
-#print('Tu edad es ' + str(edad))
+# Float
+# altura = 1.75 #float
+# print(altura)
+# print(type(altura))
 
-#Float
-#altura = 1.75 #float
-#print(altura)
-#print(type(altura))
+# print('Tu altura es: ' + str(altura))
 
-#print('Tu altura es: ' + str(altura))
-
-#Boolean
-#humano = False
+# Boolean
+# humano = False
 
 # print(humano)
 # print(type(humano))
@@ -49,6 +47,30 @@ edad += 1
 
 
 #--------------------------------------------------------
-#Clase 3
+# Clase 3 Asignaciones multiples
 #--------------------------------------------------------
 
+# nombre = 'Sebastian'
+# edad = 21
+# atractivo = True
+
+# nombre, edad, atractivo = 'Sebastian', 21, True
+
+# print(nombre)
+# print(edad)
+# print(atractivo)
+
+# nombre1 = 10
+# nombre2 = 10
+# nombre3 = 10
+# nombre4 = 10
+
+# nombre1 = nombre2 = nombre3 = nombre4 = 10
+# print(nombre1)
+# print(nombre2)
+# print(nombre3)
+# print(nombre4)
+
+# --------------------------------------------------------
+# Clase 4
+# --------------------------------------------------------
