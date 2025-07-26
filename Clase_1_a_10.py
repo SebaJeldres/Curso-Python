78f257ac996ec8dcc95b0ffbfbd97f1b12988465
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-07-25 20:54:05 -0400

    Clase 2: Tipos de datos

--- Clase_1_a_10.py
+++ Clase_1_a_10.py
@@ -1,5 +1,54 @@
+#--------------------------------------------------------
 #Clase 1
-print('Me encanta la pizza')
-print('Es muy bueno')
+#--------------------------------------------------------
+#print('Me encanta la pizza')
+#print('Es muy bueno')
 
+
+#--------------------------------------------------------
 #clase 2
+#--------------------------------------------------------
+
+#Variebles y tipos de datos
+
+#String
+#nombre = 'Sebastian' #String
+#print('Hola ' + nombre)
+#print(type(nombre)) #Muestra el tipo de dato
+
+#apellido = 'Jeldes' #String
+#nombre_completo = 'Hola ' + nombre + apellido
+#print(nombre_completo)
+
+#entero
+edad = 21 #int
+edad2 = '21'
+#edad = edad + 1
+edad += 1
+#edad2 += 1 da error
+#una es un int y lña otra es un string
+#print(type(edad))
+#print(type(edad2))
+
+#entero con cadena de texto
+#print('Tu edad es ' + str(edad))
+
+#Float
+#altura = 1.75 #float
+#print(altura)
+#print(type(altura))
+
+#print('Tu altura es: ' + str(altura))
+
+#Boolean
+#humano = False
+
+# print(humano)
+# print(type(humano))
+# print('Eres un humano: ' + str(humano))
+
+
+#--------------------------------------------------------
+#Clase 3
+#--------------------------------------------------------
+
